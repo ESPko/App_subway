08c34600b1c9830ce7a2b47cdd3d4252398a5757
Dashboard <zxzx1332@naver.com>
2025-03-20 16:24:01 +0900

    뒤로가기 버튼

--- app/app/src/main/java/com/example/app/DetailActivity.kt
+++ app/app/src/main/java/com/example/app/DetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,8 +25,12 @@ class DetailActivity : AppCompatActivity() {
             insets
         }
 
-        binding.mainBack.setOnClickListener {
+        val mainBack: ImageView = findViewById(R.id.main_back)
 
+        mainBack.setOnClickListener{
+            val intent = Intent(this@DetailActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }

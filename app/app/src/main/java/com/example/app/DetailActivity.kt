61bc27e0f2e173de1301bdd6e1a3a5ecee142473
Dashboard <zxzx1332@naver.com>
2025-03-20 14:21:36 +0900

    test중

---
+++ app/app/src/main/java/com/example/app/DetailActivity.kt
@@ -0,0 +1,30 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.app.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private val binding: ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.mainBack.setOnClickListener {
+
+        }
+    }
+}

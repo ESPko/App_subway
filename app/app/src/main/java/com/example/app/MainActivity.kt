54eacc5b32f29e4cbb69ce6a25686df451dc5551
Dashboard <zxzx1332@naver.com>
2025-03-24 12:23:45 +0900

    디테일 상단 위치 및 디자인 변경

--- app/app/src/main/java/com/example/app/MainActivity.kt
+++ app/app/src/main/java/com/example/app/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.app.databinding.ActivityMainBinding
+import com.example.app.detail.DetailActivity
 
 class MainActivity : AppCompatActivity() {
 
--- app/app/src/main/java/com/example/app/detail/DetailActivity.kt
+++ app/app/src/main/java/com/example/app/detail/DetailActivity.kt
@@ -1,4 +1,4 @@
-package com.example.app
+package com.example.app.detail
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge

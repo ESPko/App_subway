66c386311b618332056e25ab841c4c16414fe1dc
Dashboard <zxzx1332@naver.com>
2025-03-24 12:43:06 +0900

    no message

--- app/app/src/main/java/com/example/app/detail/DetailActivity.kt
+++ app/app/src/main/java/com/example/app/detail/DetailActivity.kt
@@ -1,10 +1,12 @@
 package com.example.app.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app.MainActivity
 import com.example.app.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -22,5 +24,21 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 상단 뒤로가기 버튼
+        binding.btnDetailBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        // 상단 출발 역이름 버튼
+        binding.btnDetailStart1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        // 상단 역위치 변경 버튼
+        // 상단 도착 역이름 버튼
+        // 상단 즐겨찾기 버튼
     }
 }

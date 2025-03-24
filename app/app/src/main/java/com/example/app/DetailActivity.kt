14fef5c6a720ed9a7424f1beb197b74ab7977e5a
Dashboard <zxzx1332@naver.com>
2025-03-24 09:28:29 +0900

    03.24

--- app/app/src/main/java/com/example/app/DetailActivity.kt
+++ app/app/src/main/java/com/example/app/DetailActivity.kt
@@ -1,8 +1,6 @@
 package com.example.app
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,14 +21,6 @@ class DetailActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val mainBack: ImageView = findViewById(R.id.main_back)
-
-        mainBack.setOnClickListener{
-            val intent = Intent(this@DetailActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }

af44cdafd358682fe99021b30f5c292d1f5bd573
dfdfdfd123 <winwin4668@naver.com>
2025-04-01 09:41:11 +0900

    수정3

--- app/app/src/main/java/com/example/app/jsy/StationDetailActivity.kt
+++ app/app/src/main/java/com/example/app/jsy/StationDetailActivity.kt
@@ -55,7 +55,7 @@ class StationDetailActivity : AppCompatActivity() {
 
     // 변경될 툴바 내용
     val titleTextView = TextView(this).apply {
-      text = "역 검색"
+      text = "역 정보"
       setTextColor(resources.getColor(android.R.color.white))
       textSize = 18f
       layoutParams = Toolbar.LayoutParams(
